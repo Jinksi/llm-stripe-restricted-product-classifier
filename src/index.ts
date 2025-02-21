8b55250f749e296d14e18dcd988ef7861eb1f28a
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-02-21 21:50:31 +1000

    Use clack for nicer cli

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import minimist from 'minimist'
+import { text, spinner, log } from '@clack/prompts'
 
 import { checkProductAgainstAllCriteria } from './llm'
 import { models } from './models'
@@ -40,44 +41,60 @@ const formatUrl = (url: string) => {
   return formattedUrl
 }
 
+const db = createDatabase()
+const s = spinner()
+
 // The first argument is the base URL of the WC store
 const args = minimist(process.argv.slice(2))
 const showViolations = args.show
+
+if (showViolations) {
+  // Show all violations for a site stored in the database
+  const allViolations = getAllSiteViolations(db)
+  console.log(allViolations)
+  process.exit(0)
+}
+
 const forceUpdate = args.force
 const fetchOnly = args.fetch
-
-const db = createDatabase()
 
 const main = async (
   baseUrlArg: string,
   forceUpdate: boolean,
   fetchOnly: boolean
 ) => {
-  forceUpdate && console.log('Forcing update')
+  log.info(`Checking products for ${baseUrlArg}`)
+  fetchOnly && log.info('Fetching products only')
+  forceUpdate && log.info('Forcing update')
 
   const model = models.gpt4oMini
   const baseUrl = formatUrl(baseUrlArg)
 
   const siteId = upsertSite(db, baseUrl)
 
+  s.start('Fetching products')
   const products = await fetchStoreProducts({ baseUrl })
-  console.log(`Fetched ${products.length} products`)
+  s.stop(`Fetched ${products.length} products`)
+
   products.forEach((product) => {
     // TODO: ensure this updates the product if it already exists
     upsertProduct(db, product, siteId)
   })
 
   const siteProducts = getSiteProducts(db, siteId)
-  console.log(siteProducts.map((p) => p.name))
 
   if (fetchOnly) {
-    console.log('Done')
+    log.success('Fetching products complete')
     process.exit(0)
   }
 
   for (const [index, product] of siteProducts.entries()) {
+    s.start(
+      `${index + 1}/${products.length} - Checking product ${product.name}`
+    )
+
     if (!product.description && !product.name) {
-      console.log(
+      s.stop(
         `${index + 1}/${products.length} - Skipping product ${
           product.name
         } because it has no description or name`
@@ -87,21 +104,18 @@ const main = async (
 
     const productDbResults = getProductResults(db, product.id)
     if (!forceUpdate && productDbResults.length > 0) {
-      console.log(
+      s.stop(
         `${index + 1}/${products.length} - Skipping product ${
           product.name
         } because it already has results`
       )
       continue
-    } else {
-      console.log(
-        `${index + 1}/${products.length} - Checking product ${product.name}`
-      )
     }
 
     const results = await checkProductAgainstAllCriteria(product, model, [
       'nutraceuticals',
     ])
+
     const upsertedResults = Object.entries(results.results).map(
       ([criteriaKey, result]) => {
         const resultData = {
@@ -112,19 +126,40 @@ const main = async (
       }
     )
 
-    console.log(
-      `Saved ${upsertedResults.length} results for product ${product.name}`
+    const violations = Object.entries(results.results)
+      .filter(([criteriaKey, result]) => result.violates_criteria)
+      .map(([criteriaKey, result]) => criteriaKey)
+
+    if (violations.length > 0) {
+      log.warn(
+        `Product ${product.name} has ${
+          violations.length
+        } violations: ${violations.join(', ')}`
+      )
+    }
+
+    s.stop(
+      `${index + 1}/${products.length} - Saved ${
+        upsertedResults.length
+      } results for product ${product.name}`
     )
   }
-  console.log('Done')
+
+  log.success(`Completed checking products for ${baseUrl}`)
 }
 
-if (showViolations) {
-  // Show all violations for a site stored in the database
-  const allViolations = getAllSiteViolations(db)
-  console.log(allViolations)
-  process.exit(0)
-} else {
-  // Fetch and check products
-  main(args._[0], forceUpdate, fetchOnly)
-}
+const url =
+  args._[0] ??
+  (await text({
+    message: 'What url do you want to check?',
+    placeholder: 'https://example.com',
+    validate(value) {
+      if (value.length === 0) return `Value is required!`
+      const urlPattern =
+        /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+      if (!urlPattern.test(value)) return `Please enter a valid URL`
+    },
+  }))
+
+// Fetch and check products
+main(url, forceUpdate, fetchOnly)
